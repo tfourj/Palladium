271670f919ce1443ab6c4826379fd46b2c250f33
TfourJ <48729601+tfourj@users.noreply.github.com>
2026-03-03 19:37:30 +0100

    Initial Commit

---
+++ Palladium/PalladiumApp.swift
@@ -0,0 +1,17 @@
+//
+//  PalladiumApp.swift
+//  Palladium
+//
+//  Created by TfourJ on 3. 3. 26.
+//
+
+import SwiftUI
+
+@main
+struct PalladiumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
