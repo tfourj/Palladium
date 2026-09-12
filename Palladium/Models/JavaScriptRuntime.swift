fe1b8d6a0a1e3cc134517fcc4e0c779b0c07e837
TfourJ <taj@tfourj.com>
2026-09-13 01:23:57 +0200

    add advanced javascript runtime picker

--- Palladium/Models/JavaScriptRuntime.swift
+++ Palladium/Models/JavaScriptRuntime.swift
@@ -9,6 +9,15 @@ enum JavaScriptRuntime: String, CaseIterable, Identifiable {
 
     var id: String { rawValue }
 
+    var title: String {
+        switch self {
+        case .webkit:
+            return String(localized: "settings.advanced.javascript_runtime.webkit", bundle: .app)
+        case .quickjs:
+            return String(localized: "settings.advanced.javascript_runtime.quickjs", bundle: .app)
+        }
+    }
+
     static var selected: JavaScriptRuntime {
         UserDefaults.standard.string(forKey: defaultsKey).flatMap(Self.init(rawValue:)) ?? defaultValue
     }
